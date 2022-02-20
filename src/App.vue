24090a7a9856d764c435fc306b586946b0ae3502
aurelio-sc <asc.mat@gmail.com>
2022-02-20 12:18:02 -0300

    v-for for multiple waterfalls

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <div class="waterfall">
-      <h2 class="waterfallName">{{ cachoeiraAlta.name }} </h2>
-      <img :src="cachoeiraAlta.source" :alt="cachoeiraAlta.name">
-      <p class="textContent">{{ cachoeiraAlta.location }}</p>
-      <p class="textContent">{{ cachoeiraAlta.description }}</p>
-    </div>
+    <ul class="waterfallList">
+    <li class="waterfall" v-for="waterfall of waterfalls" :key="waterfall.id">
+      <h2 class="waterfallName">{{ waterfall.name }} </h2>
+      <img :src="waterfall.source" :alt="waterfall.name">
+      <p class="textContent">{{ waterfall.location }}</p>
+      <p class="textContent">{{ waterfall.description }}</p>
+    </li>
+    </ul>
     
   </div>
 </template>
@@ -17,12 +19,23 @@ export default {
   data () {
     return {
       title: 'Cachoeiras do Espírito Santo',
-      cachoeiraAlta: {
-        source: "./src/assets/img/cachoeiraAlta.jpg",
-        name: 'Cachoeira Alta',
-        location: 'Cachoeiro de Itampemirim',
-        description: 'Com águas cristalinas e uma queda de apoximadamente 100 metros, é um ótimo local para amantes da fotografia. Contém um bar no qual é possível fazer um churrasco caso o visitante consuma no local.'
-      }
+
+      waterfalls: [
+        { 
+          name: 'Cachoeira Alta',
+          source: './src/assets/img/cachoeiraAlta.jpg',          
+          location: 'Cachoeiro de Itampemirim',
+          description: 'Com águas cristalinas e uma queda de apoximadamente 100 metros, é um ótimo local para amantes da fotografia. Contém um bar no qual é possível fazer um churrasco caso o visitante consuma no local.'
+        },
+
+        {
+          name:'Cachoeira Arco Íris',
+          source: './src/assets/img/cachoeiraArcoIris.jpg',
+          loacation: 'Divino de São Lourenço',
+          description: 'Localizada no Patrimônio da Penha, faz parte de um complexo de cascatas, juntamente com a Cachoeira do Beija-Flor e a cachoeira da Vó Tuti. Na última de suas quedas (são quatro!) possui um lindo poço que se assemelha a uma piscina. É possível acampar nas proximidades.'
+        }
+      ]
+
     }
   }
 }
